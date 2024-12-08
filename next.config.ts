dfad1ac7561c60f139d0a06e008c636a3b1b4c92
Michael <michaelraffinpaculba@gmail.com>
2024-12-08 17:39:44 +0800

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+// const withPWA = require("next-pwa");
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+// export default nextConfig;
+// module.exports = withPWA({
+//   pwa: {
+//     dest: "public", // Service worker will be generated in the `public` directory
+//     disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+//   },
+// });
+const withPWA = require("next-pwa")({
+  dest: "public", // Correct destination for the service worker
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development
+});
+
+module.exports = withPWA({});
